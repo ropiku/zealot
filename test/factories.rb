1c41c8f7beb3871b132ac28e9c53cd2178ed991a
Andrei Bocan <zmaxor@gmail.com>
2009-07-04 22:35:36 +0300

    It's a sequence, folks

--- test/factories.rb
+++ test/factories.rb
@@ -1,7 +1,7 @@
 Factory.define :user do |u|
   u.sequence(:identity_url) {|n| "user#{n}.example.com"}
   u.sequence(:nickname) {|n| "user#{n}"}
-  u.sequence(:email) {|n| "email{n}@example.com" }
+  u.sequence(:email) {|n| "email#{n}@example.com" }
 end
 
 Factory.define :spot do |s|
