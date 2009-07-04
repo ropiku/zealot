b2f326b09edf2bba6521158857b82c1d800d74fc
Andrei Bocan <zmaxor@gmail.com>
2009-07-04 12:16:39 +0300

    Fix spots

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090704085747) do
+ActiveRecord::Schema.define(:version => 20090704091504) do
 
   create_table "spots", :force => true do |t|
     t.string   "name"
