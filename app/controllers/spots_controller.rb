de4143db53aae9409f1055ff8740142380e7190e
Andrei Bocan <zmaxor@gmail.com>
2009-07-04 12:31:17 +0300

    Add spot creation

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,4 +2,18 @@ class SpotsController < ApplicationController
   def index
     @spots = Spot.all
   end
+  
+  def create
+    @spot = Spot.create(params[:spot])
+
+    respond_to do |format|
+      format.html do
+        redirect_to spot_path(@spot)
+      end
+
+      format.js do
+        head :created
+      end
+    end
+  end
 end
--- test/functional/spots_controller_test.rb
+++ test/functional/spots_controller_test.rb
@@ -10,4 +10,36 @@ class SpotsControllerTest < ActionController::TestCase
     should_render_template :index
     should_assign_to :spots
   end
+
+  context "html #create" do
+    setup do
+      post :create, :spot => { :name => "Gigel", :latitude => 10, :longitude => 20 }
+      @spot = Spot.last
+    end
+
+    should_redirect_to "created spot" do
+      spot_path(@spot)
+    end
+
+    should "create a spot" do
+      assert_equal("Gigel", @spot.name)
+      assert_equal(10, @spot.latitude)
+      assert_equal(20, @spot.longitude)
+    end
+  end
+
+  context "xhr #create" do
+    setup do
+      xhr :post, :create, :spot => { :name => "Gigel", :latitude => 10, :longitude => 20 }
+      @spot = Spot.last
+    end
+
+    should "create a spot" do
+      assert_equal("Gigel", @spot.name)
+      assert_equal(10, @spot.latitude)
+      assert_equal(20, @spot.longitude)
+    end
+
+    should_respond_with :created
+  end
 end
